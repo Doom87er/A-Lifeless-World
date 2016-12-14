17fde492b00f84f7da8c780cad245780d41a4151
Doom87er <Doom87er@Gmail.com>
2016-12-14 16:44:26 -0500

    Add files via upload

--- A LifeLess World/Assets/Cells/BasicCell/BasicCellProperty.cs
+++ A LifeLess World/Assets/Cells/BasicCell/BasicCellProperty.cs
@@ -2,54 +2,52 @@
 using System.Collections;
 
 public class BasicCellProperty : MonoBehaviour {
-	
-	//all cell property scripts MUST have this, and have a prefab with their Global stats script assigned to this value
+	/*dead code
+	public float speed = 0.0f;
+	public Vector3 cellStats.getVector() = new Vector3(0.0f,0.0f,0.0f);
+	public float energy = 0;
+	*/
 	public CellProperty_Global cellStats;
-	
-	//this value deffines what this cell evovles into
-	//can be altered but never null
 	public GameObject nextGen;
 
-	//this value MUST be included in ALL cell property scripts
-	//when true, causes the cell to update it's self the next frame
 	private bool trigUpdate = true;
 
-	/*
-	//the start function should be disabled for all property scripts
-	// if adding code here, test the upgarde to feedercell action in (OnCollisionEnter)
+	/*// if adding code here, test the upgarde to feedercell action in (OnCollisionEnter)
 	void Start () {
 	
 	}
 	*/
 	
-	//keep the update function as sparse as possible, if it dosen't need to execute for EVERY frame
-	//then put it in the trigUpdate
+	// Update is called once per frame
 	void Update () {
 		
-		//this MUST be in every cell property script
-		//updates the cell's property's when ever it interacts with something
+
 		if (trigUpdate) {
-			this.GetComponent<Rigidbody> ().velocity = (cellStats.getVector () * cellStats.getSpeed());//ensures that any acidental bumps don't move the cell of deffined course
-			cellStats.formToGrid ();//ensures that all cells line up perfectly with eachother
-			trigUpdate = false;//ensures that update trigger runs only once
+			this.GetComponent<Rigidbody> ().velocity = (cellStats.getVector () * cellStats.getSpeed());
+			cellStats.formToGrid ();
+			trigUpdate = false;
 		}
 
-		
+		/*
+		if (cellStats.getSpeed() != 0.0f) {//reduce overhead further?
+			this.transform.Translate (cellStats.getVector() * (cellStats.getSpeed() * Time.deltaTime));
+
+		}
+		*/
 	}
 
-	//all cell's must have this function to define interaction
 	void OnCollisionEnter(Collision coll){
 
-		//all cell property scripts SHOULD do this, prevents cells from doing weird things
 		if (trigUpdate == true)//one interaction at a time
 			return;
 
-		//TODO: drop tags and add an integer ID to global stats so we can use a switch statment instead of if statments
 		if(coll.gameObject.tag == "BasicCell"){
+			//GameObject cellOther = coll.gameObject;
 			
+			//BasicCellProperty cellOtherProperty = coll.gameObject.GetComponent<BasicCellProperty> ();
 			CellProperty_Global otherStats = coll.gameObject.GetComponent<CellProperty_Global>();
 
-			cellStats.setVector(cellStats.getVector() * -1);//should make this a default interaction
+			cellStats.setVector(cellStats.getVector() * -1);
 
 			if (cellStats.getEnergy() <= otherStats.getEnergy()) {//if other cell has more energy increase own energy
 				cellStats.alterEnergy(1);
@@ -70,7 +68,7 @@ public class BasicCellProperty : MonoBehaviour {
 			cellStats.setVector(cellStats.getVector() * -1);
 		}
 
-		trigUpdate = true;//scheduals this cell to update itself next frame
+		trigUpdate = true;
 	}
 	
 
